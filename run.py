948c20636659cf573c4026ef3e3b1bd123e5f3bf
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-09-05 21:29:58 +0000

    Add game intro and startGame() function

--- run.py
+++ run.py
@@ -3,7 +3,21 @@ import time
 
 
 print()
+print("That was meant to be a holiday of your lifetime.")
+print("Everything went well at first... ")
 print()
+time.sleep(3)
+print("...until the turbulence started...")
+print()
+time.sleep(3)
+print("...followed by darkness...")
+time.sleep(3)
+print()
+print("You wake up after what feels like hours,")
+print("only to realise your plane crashed and you are...")
+print()
+print()
+time.sleep(4)
 print("     ##############################")
 print("     #                            #")
 print("     #          STRANDED          #")
@@ -12,8 +26,20 @@ print("     ##############################")
 print()
 print()
 time.sleep(3)
-print("That was meant to be a holiday of your lifetime.")
-print("Everything went well at first... ")
-print()
-time.sleep(3)
-print("... until the turbulence started....")
+
+
+def startGame():
+    leaveIsland = input("Do you want to find a way off the island?: (yes/no) ")
+    if leaveIsland == "no":
+        print()
+        print("Well, it was nice knowing you...")
+        print("Enjoy a slow and painful death from dehydration.")
+    elif leaveIsland == "yes":
+        print()
+        name = input("Great! Do you remember who you are? (type name) ")
+    else:
+        print()
+        print("Wrong input. Please type 'yes' or 'no'.")
+
+
+startGame()
